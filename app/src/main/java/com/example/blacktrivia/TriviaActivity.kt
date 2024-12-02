99bacb5fb09bd716814fd9eef9fae1bf0f3a6543
kwright29 <khloewright329@gmail.com>
2024-12-01 20:49:21 -0600

    Adding bottom navigation view for trivia and profile

--- app/src/main/java/com/example/blacktrivia/TriviaActivity.kt
+++ app/src/main/java/com/example/blacktrivia/TriviaActivity.kt
@@ -2,25 +2,19 @@ package com.example.blacktrivia
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class ProfileActivity : AppCompatActivity() {
+class TriviaActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
-
-        val textViewGreeting = findViewById<TextView>(R.id.textViewGreeting)
-
-        // Retrieve the username from the intent
-        val username = intent.getStringExtra("USERNAME")
-        textViewGreeting.text = "Welcome, $username!"
+        setContentView(R.layout.activity_trivia)
 
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
-        bottomNavigationView.selectedItemId = R.id.menu_profile
+        bottomNavigationView.selectedItemId = R.id.menu_trivia
 
-        bottomNavigationView.setOnItemSelectedListener {  menuItem ->
+        bottomNavigationView.setOnItemSelectedListener { menuItem ->
             when (menuItem.itemId) {
                 R.id.menu_trivia -> {
                     if (this::class != TriviaActivity::class) {
